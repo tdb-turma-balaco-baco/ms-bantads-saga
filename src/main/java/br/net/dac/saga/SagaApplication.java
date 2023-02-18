9992ee4fe4b85f5c17c5864b0071832059e85b3b
mateus <imw.desenv@gmail.com>
2023-02-18 19:57:55 -0300

    Initial files

---
+++ src/main/java/br/net/dac/saga/SagaApplication.java
@@ -0,0 +1,13 @@
+package br.net.dac.saga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaApplication.class, args);
+	}
+
+}
